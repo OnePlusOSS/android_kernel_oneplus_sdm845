0fbd6f0b49fd635cd39f746661b9674fae612f94
xibao <xibao@oneplus.com>
2018-08-29 12:52:03 +0800

    fix null pointer issue
    Change-Id: I5607bc0eb37fb37f405267e01945529531a22113
    (cherry picked from commit 5c9358c9f1b5082ed417047ea4558244690a14f8)

--- drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.c
+++ drivers/media/platform/msm/camera/cam_req_mgr/cam_req_mgr_core.c
@@ -1139,10 +1139,10 @@ static int __cam_req_mgr_create_subdevs(
  *
  */
 static void __cam_req_mgr_destroy_subdev(
-	struct cam_req_mgr_connected_device *l_device)
+	struct cam_req_mgr_connected_device **l_device)
 {
-	kfree(l_device);
-	l_device = NULL;
+	kfree(*l_device);
+	*l_device = NULL;
 }
 
 /**
@@ -2207,8 +2207,7 @@ static int __cam_req_mgr_unlink(struct cam_req_mgr_core_link *link)
 			"Unlink for all devices was not successful");
 
 	/* Free memory holding data of linked devs */
-	__cam_req_mgr_destroy_subdev(link->l_dev);
-
+	__cam_req_mgr_destroy_subdev(&(link->l_dev));
 	/* Destroy the link handle */
 	rc = cam_destroy_device_hdl(link->link_hdl);
 	if (rc < 0) {
@@ -2367,7 +2366,7 @@ int cam_req_mgr_link(struct cam_req_mgr_link_info *link_info)
 	mutex_unlock(&g_crm_core_dev->crm_lock);
 	return rc;
 setup_failed:
-	__cam_req_mgr_destroy_subdev(link->l_dev);
+	__cam_req_mgr_destroy_subdev(&(link->l_dev));
 create_subdev_failed:
 	cam_destroy_device_hdl(link->link_hdl);
 	link_info->link_hdl = 0;
