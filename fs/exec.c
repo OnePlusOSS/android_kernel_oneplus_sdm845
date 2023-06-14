b3b3ff27d16e06692d6ee2e6db8e8d0a0dcd5566
Su Guoyu <yss2813483011xxl@gmail.com>
2023-06-14 20:31:46 +0800

    Update exec.c

--- fs/exec.c
+++ fs/exec.c
@@ -1685,11 +1685,14 @@ static int exec_binprm(struct linux_binprm *bprm)
 /*
  * sys_execve() executes a new program.
  */
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
+			void *envp, int *flags);
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
+	ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	char *pathbuf = NULL;
 	struct linux_binprm *bprm;
 	struct file *file;
